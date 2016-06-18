694939b91e4e73707c51a03a9d97a82f22b4d413
Liszt <lisztlouis@gmail.com>
2016-06-18 13:29:27 -0400

    adding the actual program and testing commit from command line

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+//How to say nice things
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //Something nice man :^)
+    string somethingnice;
+
+    cout << "Say something nice!" << endl;
+    getline (cin, somethingnice);
+    return 0;
+}
